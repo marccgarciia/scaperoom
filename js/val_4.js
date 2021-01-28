25b791b595a6adc1d31b0c2f4dedac8d45d074da
Cristian <100005774.joan23@fje.edu>
2021-01-28 12:23:08 +0100

    Cristian 3

--- js/val_4.js
+++ js/val_4.js
@@ -1,9 +1,10 @@
 function val_1() {
-    var a = document.getElementById('val-1').value
-    if (a == 'A') {
+    var a = document.getElementById('p4_1').value
+    var b = document.getElementById('p4_2').value
+    if (a == '11' && b == 'ron y hermione') {
         return true
     } else {
-        alert('¡Inténtalo de nuevo Crack!')
+        alert('¡Inténtalo de nuevo Muggle!')
         return false
     }
 }
