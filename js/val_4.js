e1a010d0de0dd7116128fd59f3eb93d78268b712
Cristian <100005774.joan23@fje.edu>
2021-02-02 15:54:31 +0100

    Cest fini

--- js/val_4.js
+++ js/val_4.js
@@ -8,3 +8,25 @@ function val_1() {
         return false
     }
 }
+
+function val_2() {
+    var a = document.getElementById('p4_3').value
+    var b = document.getElementById('p4_4').value
+    if (a == 'ron' && b == 'dumbeldore') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo Muggle!')
+        return false
+    }
+}
+
+function val_3() {
+    var a = document.getElementById('p4_5').value
+    var b = document.getElementById('p4_6').value
+    if (a == 'ollivanders' && b == 'hipogrifo') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo Muggle!')
+        return false
+    }
+}
